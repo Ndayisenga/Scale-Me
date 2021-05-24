5e807b4f664b7617a56ddfd54d952747c758d9ea
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-24 15:00:02 +0200

    Initial Commit

---
+++ ScaleMe/ScaleMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleMeApp.swift
+//  ScaleMe
+//
+//  Created by Ndayisenga Jean Claude on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScaleMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
